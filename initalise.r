203b381599b69a4e371623da01be92067f10e27e
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-11-01 10:54:06 +0000

    installing required packages

---
+++ initalise.r
@@ -0,0 +1,14 @@
+install.packages('devtools')
+library(devtools)
+
+install_github('douglask3/benchmarkmetrics/benchmarkMetrics')
+install_bitbucket('douglask3/gitProjectExtras/gitBasedProjects')
+install.packages('raster')
+install.packages(ncdf4)
+install_bitbucket('douglask3/rasterExtraFuns/rasterExtras')
+install_bitbucket('douglask3/rasterExtraFuns/rasterPlotFunctions')
+install.packages(plotrix)
+install.packages('mapdata')
+install.packages(mapplots)
+install.packages(ellipse)
+install.packages(raster) ## package for reading in model output
