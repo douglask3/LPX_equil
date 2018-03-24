af1f5834b56aff5547920aec4023ed1008c585d4
Douglas Kelley <douglas.i.kelley@gmail.com>
2018-03-24 10:06:36 +0000

    tidying and commenting

--- plot_basic_vars.r
+++ plot_basic_vars.r
@@ -8,15 +8,23 @@ fname_in = '/Users/dougl/Dropbox/LPX_storage_shed/Equilibrium Test 2 PI - 201709
 
 
 ## varnames in fname_in
-varnames = c(height = 'height', fpc = 'fpc_grid', gdd = 'gdd_grid', lai = 'lai_ind')
+varnames = c(height = 'height'  ,
+			 fpc    = 'fpc_grid', 
+			 gdd    = 'gdd_grid', 
+			 lai    = 'lai_ind' )
 
 ## biome colour for plotting - same order as biome key below.
-cols = list(height = c("white", "#333300"), fpc = c("white", "#99FF00", "#003300"), gdd = c("white", "#AABB00", "#220000"),
-		   lai = c("white", "brown", "#002200"))
+cols = list(height = c("white", "#333300"),
+		    fpc    = c("white", "#99FF00", "#003300"), 
+			gdd    = c("white", "#AABB00", "#220000"),
+		    lai    = c("white", "brown", "#002200"))
 		   
 dcols = list( gdd = c("#000022", "#88FF88", "white", "#DDDD00", "#220000"))
 		 
-limits = list(height = c(2, 4, 6, 8, 10, 12, 14, 16, 18, 20), fpc = c(0.2, 0.4, 0.6, 0.8, 0.9, 0.95), gdd = seq(0, 8000, 1000), lai = c(0, 0.2, 0.5, 1, 2, 4, 6, 8, 10))
+limits = list(height = c(2, 4, 6, 8, 10, 12, 14, 16, 18, 20), 
+			  fpc    = c(0.2, 0.4, 0.6, 0.8, 0.9, 0.95), 
+			  gdd    = seq(0, 8000, 1000), 
+			  lai    = c(0, 0.2, 0.5, 1, 2, 4, 6, 8, 10))
 ##################################
 ## open							##
 ##################################
@@ -24,13 +32,16 @@ dat = lapply(varnames, function(i) stack(fname_in, varname = i))
 
 weightByFPC <- function(x)
 	sum(layer.apply(1:9,  function(i) dat[[x]][[i]] * dat[['fpc']][[i]]))
-## fpc tree, grass etc
+
 dat[["lai"   ]] = weightByFPC('lai'   )
 dat[["height"]] = weightByFPC('height')
 dat[['fpc'   ]] = sum(dat[['fpc']])
 				   
 dat = lapply(dat, convert_pacific_centric_2_regular)
 
+##################################
+## plot							##
+##################################
 plotMap <- function(dat, limits, cols, vname) {
 	dat[dat > 9E9] = NaN
 	
