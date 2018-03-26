4bc3b14b6621f6087dceff1bbbd85bc64df112f8
Douglas Kelley <douglas.i.kelley@gmail.com>
2018-03-26 14:59:12 +0100

    moving some funs to libs and opening own data

---
+++ libs/grad.raster.r
@@ -0,0 +1,21 @@
+
+grad.raster <- function(r) {
+	nr = nrow(r)
+	nc = ncol(r)
+
+	v = matrix(r[], nrow = nc)
+
+	v = list(v[c(2:nc, 2), ],
+				 v[c(nc, 1:(nc-1)), ],
+				 v[, c(2:nr, 1)],
+				 v[, c(nr, 1:(nr-1))])
+				
+	d = layer.apply(v, function(i) {r[] = as.vector(i); r})
+
+	m = 1.1 * mean(addLayer(r, d), na.rm = TRUE)
+	return(abs(m - r))
+}
+
+grad.brick <- function(b) layer.apply(b, grad.raster)
+
+mmGrad.brick <- function(b, ...) sum(grad.brick(b, ...)) / nlayers(b)
--- plot_spatial_MM.r
+++ plot_spatial_MM.r
@@ -1,35 +1,33 @@
-source("plot_fracCovers.r")
+##################################
+## cfg							##
+##################################
+source("cfg.r")
 
-dats = lapply(dats, function(i) i[[1]])
+## LPX output files containing fpc_grid
+fname_in = c(PI = '/Users/dougl/Dropbox/LPX_storage_shed/Equilibrium Test 2 PI - 20170912 (CASP)/test_R20C_CRUnonclim_PI1-4099.nc',
+		     Paleo = '/Users/dougl/Dropbox/LPX_storage_shed/Equilibrium Test 2 PI - 20170912 (CASP)/test_R20C_CRUnonclim_PI1-1000.nc')
 
+varname    = 'fpc_grid'
 
-grad.raster <- function(r) {
-	nr = nrow(r)
-	nc = ncol(r)
-
-	v = matrix(r[], nrow = nc)
-
-	v = list(v[c(2:nc, 2), ],
-				 v[c(nc, 1:(nc-1)), ],
-				 v[, c(2:nr, 1)],
-				 v[, c(nr, 1:(nr-1))])
-				
-	d = layer.apply(v, function(i) {r[] = as.vector(i); r})
-
-	m = 1.1 * mean(addLayer(r, d), na.rm = TRUE)
-	return(abs(m - r))
+## Define cols and limits for plotting
+cols   = c("white", "green", "#220000")
+limits = c(0, 0.01, 0.02, 0.04, 0.06, 0.08, 0.1)
+			  
+##################################
+## open							##
+##################################
+open_data <- function(fname) {
+	dat = stack(fname, varname = varname)
+	dat = convert_pacific_centric_2_regular(dat)
+	dat[dat > 9E9] = NaN	
+	return(dat)
 }
-
-grad.brick <- function(b) layer.apply(b, grad.raster)
-
-mmGrad.brick <- function(b, ...) sum(grad.brick(b, ...)) / nlayers(b)
+dats = lapply(fname_in, open_data)
 
 plotGrad <- function(dat, mn) {
-	dat = convert_pacific_centric_2_regular(dat)
-	dat[dat > 9E9] = NaN
-	mm = mmGrad.brick(dat)
-	
-	plot_SA_Map_standard(mm, mn, c(0, 0.01, 0.02, 0.04, 0.06, 0.08, 0.1), c("white", "green", "#220000"))
+	grad = mmGrad.brick(dat)
+	plot_SA_Map_standard(grad, mn, limits, cols)
 }
+
 par(mfrow = c(1, 2), mar = rep(0, 4))
 mapply(plotGrad, dats, names(dats))
