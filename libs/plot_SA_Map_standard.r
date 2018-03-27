82564b8b7b34d8d874364d2269036c3cfed114d7
Douglas Kelley <douglas.i.kelley@gmail.com>
2018-03-27 14:34:29 +0100

    shifting map name down a bit when plotting SA map

--- libs/plot_SA_Map_standard.r
+++ libs/plot_SA_Map_standard.r
@@ -4,5 +4,5 @@ plot_SA_Map_standard <- function(dat, vname = '', limits, cols, add_legend = TRU
 	if (is.infinite(min.raster(dat, na.rm = TRUE))) dat[] = 0.0
 	plot_raster_from_raster(dat, limits = limits, cols = cols, quick = TRUE, plot_loc = c(0.7, 1.35, -0.4,	-0.35),
 						add_legend = add_legend)
-	mtext(vname, side = 1, adj = 0.85, line = -5, font = 1.67)
+	mtext(vname, side = 1, adj = 0.85, line = -3, font = 1.67)
 }
