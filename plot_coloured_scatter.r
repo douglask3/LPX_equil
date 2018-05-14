bfdde25080e39a1959c70025b4e7b3ff7cae29e7
Douglas Kelley <douglas.i.kelley@gmail.com>
2018-05-14 14:17:06 +0100

    adding comments

--- plot_coloured_scatter.r
+++ plot_coloured_scatter.r
@@ -37,18 +37,18 @@ z_cols     = list(c('black', 'yellow', 'green'), ## The colour map stages for ou
                                                ## The first colour (black) is what we want to colour point with no tree cover/fire. 2nd (yellow) is intermediate and 3rd (green or red) is for highest values of tree or fire.
 
 z_levels   = list(c(1, 2, 5, 10, 20, 40, 60, 80), c(0.01, 0.02, 0.05, 1, 2, 5, 10)) ## Levels cut offs for colors. See plot legend.
-##########
-## open ##
-##########
+###############
+## open data ##
+###############
 
 openDat <- function(filename, scale = 1, layers = NULL, ...) {
     filename = paste(data_dir, filename, sep = '/')
-    r = brick(filename, ...)
+    r = brick(filename, ...) ## load data
     
-    if (!is.null(layers)) r = r[[layers]]
-    if (nlayers(r) > 1) r = mean(r) else r = r[[1]]
-    r = r * scale
-    r[r > 9E9] = NaN
+    if (!is.null(layers)) r = r[[layers]] ##reqaired layers (i.e, will load layers 1:7 for fpc to get tree cover
+    if (nlayers(r) > 1) r = mean(r) else r = r[[1]] ## meaning remaining layers
+    r = r * scale ## scaling
+    r[r > 9E9] = NaN ## setting mask
     return(r)
 }
 
@@ -59,8 +59,13 @@ z = mapply(openDat, z_file, z_scale, z_layers, varname = z_varnames)
 ##########
 ## plot ##
 ##########
+## set up plotting widnow.
+    # mfcol = c(ncol, nrow) is the plotting grid.
+    # mar = c(bottom, left, top, right) is the inner margin (i.e, gap around each plot).
+    # oma is the outer margin (gap around the entire grid).
 par(mfcol = c(length(z), length(x)), mar = c(3.5, 0.1, 0, 1), oma = c(2, 5, 1, 0))
 
+## Function foradding a legend.
 Legend <- function(zlevel, zcol, zname) {
     labs = paste(head(zlevel, -1), zlevel[-1], sep = '-')
     labs = c(paste("<", zlevel[1]),
@@ -69,38 +74,56 @@ Legend <- function(zlevel, zcol, zname) {
     legend(x = 'topleft', legend = labs, pch = 19, col = zcol, ncol = 3, title = zname, bty = 'n')
 }
 
+## plotting function for individual plot in our grid
 plot3scatter <- function(title, x, xname, y, yname, addLegend, z, zcol, zlevel, zname, yaxt, ...) {
     
+    ## remove values not on a common mask
     mask = !is.na(x + y + z)
     x = x[mask]
     y = y[mask]
     z = z[mask]
+
+    # make colours a bit transparent and assign a colour to each z datapoint
     z = cut_results(z, zlevel)
-    
     zcol_plot = make.transparent(zcol, 0.9)
     zcol_plot = zcol_plot[z]
+
+    # make plotting area and fill with black points
     plot(x, y, pch = 19, xlab  = '', ylab = '', yaxt = yaxt, ...)
+
+    # add our coloured points, at 20 different sizes so we can see detail in areas with lots of points
     for (cex in seq(1.0, 0.05, -0.05)) points(x, y, pch = 19, col = zcol_plot, cex = cex)
 
+    # add various lables
     mtext(side = 1, xname, line = 2.5)
     if (yaxt == 's') {
         mtext(side = 2, title, line = 3.5, cex = 1.3)
         mtext(side = 2, yname, line = 2.5)
     }
-
+    
+    # add legend when asked
     if(addLegend) Legend(zlevel, zcol, zname)
    
 }
 
+# plots all plots withb same colopur variable
 plotZs <-function(z, zcol, zlevel, zname, ...) {
+    # convert our colour chose for our z color variable into a full colour map
     zcol =  make_col_vector(zcol, ncols = length(zlevel) + 1) 
+
+    # descide which of the x-y plots will have our color legend
     addLegend = rep(FALSE, length(x))
     addLegend[1] = TRUE
+
+    # plots all the plots of this colour
     mapply(plot3scatter, titles, x, x_names, y, y_names, addLegend, MoreArgs = list(z, zcol, zlevel, zname, ...))
 }
 
+# decide which of our plots will have a y axis added
 yaxt = rep('n', length(z))
 yaxt[1] = 's'
+
+# plots all the plots
 mapply(plotZs, z, z_cols, z_levels, z_names, yaxt = yaxt)
 
 #z, z_cols, z_levels
