a93c0babbca5c73c9bd219144ed98a588e94a82e
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-01-23 17:31:16 +0000

    making proper sea mask rather than outline sea mask

--- data/make_coasline.r
+++ data/make_coasline.r
@@ -8,18 +8,18 @@ icemask = dat == 2
 
 dat[dat > 1] = 1
 
-vdat = as.matrix(dat)
+#vdat = as.matrix(dat)
 
-nr = nrow(vdat)
-nc = ncol(vdat)
+#nr = nrow(vdat)
+#nc = ncol(vdat)
 
-dat_x = dat_y =  dat
-dat_x[] = abs(vdat - vdat[c(2:nr,1),])
-dat_y[] = abs(vdat - vdat[,c(2:nc,1)])
+#dat_x = dat_y =  dat
+#dat_x[] = abs(vdat - vdat[c(2:nr,1),])
+#dat_y[] = abs(vdat - vdat[,c(2:nc,1)])
 
-dat[] = vdat
+#dat[] = vdat
 
-seamask = (dat_x + dat_y) > 0
+seamask = dat#(dat_x + dat_y) > 0
 
-writeRaster(icemask, 'data/icemask.nc')
-writeRaster(seamask, 'data/seamask.nc')
+writeRaster(icemask, 'data/icemask.nc', overwrite = T)
+writeRaster(seamask, 'data/seamask.nc', overwrite = T)
