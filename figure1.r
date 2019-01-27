0cea6b34ead48c39b730bb280ef36cb2d87b73b2
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-01-27 10:12:15 +0000

    moving standard plot scipt to libs

--- figure1.r
+++ figure1.r
@@ -12,27 +12,7 @@ source("libs/load_biome_outs_and_info.r")
 limits = seq(1.5, length.out = length(cols) - 1)
 
 
-plot_map_standrd <- function(r, cols, limits, ...) {
-    
-    plot_map <- function(colsi, ...) 
-        plot_raster_from_raster(r, cols = colsi, limits = limits, quick = TRUE, readyCut = TRUE, coast.lwd = NULL, add_legend = FALSE,...)
-    
-    openResampleMask <- function(file, ...) {
-        mask = raster(file)
-        mask = raster::resample(mask, r)
-        mask = disaggregate(mask, 5)
-    }
-    
-    plot_map(cols)
-    
-    mask = openResampleMask('data/seamask.nc')
-    contour(mask, add = TRUE, drawlabels = FALSE, lwd = 0.1)
 
-    plot_map(cols = make.transparent(cols,0.5), add = TRUE)
-
-    mask = openResampleMask('data/icemask.nc')
-    plot_raster_from_raster(mask, col = c('transparent', '#CCCCCC'), limits = c(0.5), coast.lwd = NULL, add = TRUE, quick = TRUE, add_legend = FALSE)
-}
 
 plot_biomes <- function(r, name) {    
     plot_map_standrd(r, cols, limits)
---
+++ libs/plot_map_standrd.r
@@ -0,0 +1,21 @@
+plot_map_standrd <- function(r, cols, limits, readyCut = TRUE, ...) {
+    
+    plot_map <- function(colsi, ...) 
+        plot_raster_from_raster(r, cols = colsi, limits = limits, quick = TRUE, readyCut = readyCut, coast.lwd = NULL, add_legend = FALSE,...)
+    
+    openResampleMask <- function(file, ...) {
+        mask = raster(file)
+        mask = raster::resample(mask, r)
+        mask = disaggregate(mask, 5)
+    }
+    
+    plot_map(cols)
+    
+    mask = openResampleMask('data/seamask.nc')
+    contour(mask, add = TRUE, drawlabels = FALSE, lwd = 1)
+
+    #plot_map(cols = make.transparent(cols,0.67), add = TRUE)
+
+    mask = openResampleMask('data/icemask.nc')
+    plot_raster_from_raster(mask, col = c('transparent', '#CCCCCC'), limits = c(0.5), coast.lwd = NULL, add = TRUE, quick = TRUE, add_legend = FALSE)
+}
