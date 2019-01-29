e5e9bfc9cb8e85ee3aa006302861bc45a1ffce43
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-01-29 22:11:49 +0000

    plotting to png instead

--- figure1.r
+++ figure1.r
@@ -11,13 +11,15 @@ plot_biomes <- function(r, name) {
 }
 
 plotFigure <- function(dat, name) {
-    fname = paste0('figs/Figure1', name, '.pdf')
-    pdf(fname, width = 5, height = 5)
-    par(mfrow = c(2,2), mar = rep(0, 4))
-    bv = mapply(plot_biomes, dat, names(files))
-    bv = sort(unique(unlist(bv)))
-    cols = c(cols[bv], 'ice' = "#CCCCCC")
-    legend('left', col = cols, legend = names(cols), pch = 15, ncol = 2, pt.cex = 3)
+    fname = paste0('figs/Figure1', name, '.png')
+    png(fname, height = 7, width = 5, res = 300, units = 'in')
+        layout(rbind(1:2, 3:4, 5), heights = c(1,1, 0.3))
+        par( mar = rep(0, 4), oma = c(0, 0, 0, 0.5))
+    
+        bv = mapply(plot_biomes, dat, names(files))
+        bv = sort(unique(unlist(bv)))
+        cols = c(cols[bv], 'ice' = "#CCCCCC")
+        legend('left', col = cols, legend = names(cols), pch = 15, ncol = 2, pt.cex = 3)
     dev.off()
 }
 
