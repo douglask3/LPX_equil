cda944f8fd822b3dc531fb2c27f0196cff1f3a39
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-11-13 10:11:15 +0000

    plotting individual models biom maps

--- figure1.r
+++ figure1.r
@@ -1,5 +1,7 @@
 graphics.off()
 source("cfg.r")
+library(raster)
+
 
 site_file = 'data/data_model_comparison_DMM_with_master_corelist.csv'
 
@@ -22,17 +24,42 @@ plot_biomes <- function(r, name, tpoints = TRUE) {
 }
 
 plotFigure <- function(dat, name, tpoints = TRUE) {
-    fname = paste0('figs/Figure1', name, '.png')
-    png(fname, height = 7, width = 5.35, res = 300, units = 'in')
+    fname = paste0('figs/Figure1', name, '.pdf')
+    print(fname)
+    pdf(fname, height = 7, width = 5.35)#, res = 300, units = 'in')
         layout(rbind(1:2, 3:4, 5), heights = c(1,1, 0.3))
         par( mar = rep(0, 4), oma = c(0, 0, 0, 0.5))
     
         bv = mapply(plot_biomes, dat, names(files), c(T, rep(tpoints, 3)))
         bv = sort(unique(unlist(bv)))
         cols = c(cols[bv], 'ice' = "#CCCCCC")
+        colsi = unique(cols) 
+        index = sapply(colsi, function(i) which(cols == i)[1])
+        cols = cols[index]
         legend('left', col = cols, legend = names(cols), pch = 15, ncol = 2, pt.cex = 3)
     dev.off()
+    print("plotted")
 }
 
-plotFigure(dat, '')
-plotFigure(ddat, '-diff', FALSE)
+PlotFigures <- function(dat, ddat, fname = '', tpoints = TRUE) {
+    plotFigure(dat, fname, tpoints)
+    plotFigure(ddat, paste0('-diff', fname), FALSE)
+}
+
+c(dat, ddat, files) := load_biomes(quick = FALSE)
+PlotFigures(dat, ddat)
+
+biome_dir = 'data/4_models_raw_output/'
+
+forModel <- function(name) {
+    
+    biome_files = paste0(name, c("_pico2_FOFF", "_pico2_FON", "_foff", "_fon"), ".nc")
+    names(biome_files) = c("a) control", "b) fire only", "c) low [~CO2~]\nonly", "d) fire &\nlow [~CO2~]")
+
+    tas_file = 'data/Figures_doug/Figure 2_6/LGM_R20C2_detr_ensemble_hdx_tmp_ave_cropped.nc'
+
+    c(dat, ddat) := load_biomes(biome_dir, biome_files, tas_file, quick = FALSE)
+    PlotFigures(dat, ddat, name, FALSE)
+}
+
+lapply(c("HAD", "CNRM", "FGOALS", "MIROC"), forModel)
