96255a5a8d859b3a70305f0f363323c50bde1ca0
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-11-13 09:31:37 +0000

    allowing raster, brick, etc opening and some cropping after open

--- libs/openMaskVals.r
+++ libs/openMaskVals.r
@@ -1,5 +1,7 @@
-openMaskVals <- function(file, ..., FUN = raster) {
-    r = FUN(file, ...)
+openMaskVals <- function(file, ..., extent = NULL, fun = NULL, FUN = raster) {
+    if (is.null(fun)) fun = FUN
+    r = fun(file, ...)
+    if (!is.null(extent)) r = raster::crop(r, y = extent)
     r[r > 9E9] = NaN
     return(r)
 }
