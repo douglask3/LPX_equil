1b3c156b347602107ae11019a186a53c7ca0d0de
Douglas Kelley <douglas.i.kelley@gmail.com>
2021-05-25 16:39:19 +0100

    starting connectivity score calculation workslow for just one zscore for now. Two stages: 1) straight line connectivity; 2) targgeted randon walk starting at best score a radnomly choosed distance from origin. Applied to pre and post corrected fpc zscores and a dirty plot thing at the end

--- correct_variables.r
+++ correct_variables.r
@@ -33,6 +33,11 @@ dcolss = list(rev(c('#8c510a','#bf812d','#dfc27d','#f6e8c3','#f5f5f5','#c7eae5',
 zlimits =  c(-4, -3, -2, -1, -0.1, 0.1, 1, 2, 3, 4)
 zcols = rev(c('#7f3b08','#b35806','#e08214','#fdb863','#fee0b6','#f7f7f7','#d8daeb','#b2abd2','#8073ac','#542788','#2d004b'))
 
+
+conn_cols = c('#ffffe5','#f7fcb9','#d9f0a3','#addd8e',
+              '#78c679','#41ab5d','#238443','#006837','#004529')
+conn_limits = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)  
+
 #################
 ## open things ##
 #################
@@ -146,8 +151,165 @@ apply2Var <- function(varname, levels, aggFUN, limits, cols, dlimits1, dlimits2,
 ##############################
 ## Connectivity score       ##
 ##############################
-zscores = mapply(apply2Var,  varnames, levelss, aggFUNs,
-                 limitss, colss, dlimitss1, dlimitss2, dcolss,
-                  transs, itranss)
+#zscores = mapply(apply2Var,  varnames, levelss, aggFUNs,
+#                 limitss, colss, dlimitss1, dlimitss2, dcolss,
+#                  transs, itranss)
+
+zscore1 = zscores[[1]][[5]]
+zscore2 = zscores[[2]][[5]] 
+zscore1 = crop(zscore1, extent(c(-83, -33, -30, 15)))      
+zscore2 = crop(zscore2, extent(c(-83, -33, -30, 15)))      
+start = c(-70.25, 8.25)
+
+j0 = colFromX(zscore, start[1])
+i0 = rowFromY(zscore, start[2])
+#conn = addLayer(zscore, zscore, zscore, zscore)
+#conn[] = NaN
+
+#conn[[1]][i0,j0] = 0
+#conn[[2]][i0,j0] =  zscore[i0,j0]
+#conn[[3]][i0,j0] =  zscore[i0,j0]
+#conn[[4]][] = 0
+#conn[[4]][is.na(zscore)] = NaN
+
+iijj = lapply(c(-1, 0, 1), function(jj)
+                       sapply(c(-1, 0, 1), function(ii,jj) c(ii,jj), jj)) 
+iijj = do.call(cbind, iijj)
+
+findM <- function(i, j, conn, rin, test0 = FALSE)  {
+        newRange <- function(ij) {
+            #if (is.na(conn[[1]][ij[1],ij[2]])) return(c(NaN, NaN))
+            c(min(conn[[1]][ij[1],ij[2]], rin[i, j]),
+              max(conn[[2]][ij[1],ij[2]], rin[i, j]))
+        }
+        whichMinD  <- function(v) {
+            if (all(is.na(v))) return(c(NaN, NaN, NaN))
+            d = v[2,] - v[1,]
+            if (test0 && !is.na(conn[[3]][i,j]) && conn[[3]][i,j]<=min(d)) return(NULL)    
+            index = which.min(d)
+            c(v[,index], d[index])
+        }
+        iijj = iijj + c(i,j)
+        test = iijj[1,] <nrow(conn[[3]]) & iijj[2,] <ncol(conn[[3]])
+        iijj = iijj[,test]
+        iijj = t(iijj[,!is.na(apply(iijj, 2, function(i) conn[[1]][i[1], i[2]]))])
+        #if (min(dim(iijj))>1) browser()
+        out =  whichMinD(apply(iijj, 1, newRange))
+        if (is.null(out)) return(conn)
+        #out = lapply(c(-1, 0, 1)+j, function(jj) sapply(c(-1, 0, 1)+i, newRange, jj))  
+        #out = whichMinD(do.call(cbind, out)    )
+        #finRange <- function(r, FUN)
+        #     FUN(c(rin[i,j], r[(i-1):(i+1), (j-1):(j+1)]), na.rm = TRUE)
+        #min = finRange(conn[[1]], min)
+        #max = finRange(conn[[1]], max)          
+        conn[[1]][i,j] = out[1]
+        conn[[2]][i,j] = out[2] 
+        conn[[3]][i,j] = out[3]   
+        conn
+} 
+initaliseRW <- function(rin) {
+    conn = as.matrix(rin)
+    conn[,] = NaN
+    conn = list(conn, conn, conn, conn)
+    conn[[1]][i0,j0] =  rin[i0,j0]
+    conn[[2]][i0,j0] =  rin[i0,j0] 
+    conn[[4]][,] = 0.0 
+
+    nr = nrow(rin); nc = ncol(rin)
+    maxDist =  max(i0, j0, nr-i0, nc-j0)
+    
+    for (d in 1:maxDist) {
+        print(d/maxDist)
+        indexicate <- function(k) 
+            rbind(c(i0-d, j0-k), c(i0+d, j0-k), c(i0-d, j0+k), c(i0+d, j0+k),
+                  c(i0-k, j0-d), c(i0+k, j0-d), c(i0-k, j0+d), c(i0+k, j0+d))
+    
+        index = lapply(0:d,  indexicate)
+        index = unique(do.call(rbind, index))
+        index = index[apply(index>0, 1, all) & index[,1] < nr & index[,2] < nc,]
+    
+        order = sample(1:nrow(index), nrow(index), replace = FALSE)
+        for (s in order) {
+            if (is.na(rin[index[s, 1], index[s, 2]])) next
+            conn = findM(index[s, 1], index[s, 2], conn, rin)#, conn[[1]], max)   
+            #conn[[2]] = findM(index[s, 1], index[s, 2])#, conn[[2]], min)  
+            conn[[4]][index[s, 1], index[s, 2]] = conn[[4]][index[s, 1], index[s, 2]]+1    
+        }
+        #if (d == 22) browser()
+    }
+    rout = addLayer(rin, rin, rin, rin)
+    for (i in 1:4) rout[[i]][] = conn[[i]]
+    return(rout)   
+}
+conn1 = initaliseRW(zscore1)   
+conn2 = initaliseRW(zscore2)
 
 
+browser()
+targetRW <- function(rin, zscore, ninter) {
+    conn = layer.apply(rin, function(i) as.matrix(i))
+    nr = nrow(rin); nc = ncol(rin)
+    maxDist = max(c(sqrt(i0^2 + j0^2), sqrt((i0-nr)^2 + j0^2),
+                  sqrt((i0-nr)^2 + (j0-nc)^2), sqrt(i0^2 + (j0 - nc)^2)))
+    for (i in 1:ninter) {
+        d = runif(1, 0, maxDist)
+        print(i)
+        FUN <- function(j) {
+            i = round(sqrt(d^2 - j^2))
+            rbind(c(i0-i, j0-j), c(i0-i, j0+j), c(i0+i, j0-j), c(i0+i, j0+j),
+                  c(i0-j, j0-i), c(i0-j, j0+i), c(i0+j, j0-i), c(i0+j, j0+i))
+            
+        }
+        index = lapply(0:d, FUN)
+        index = unique(do.call(rbind, index))
+        index = index[apply(index>0, 1, all) & index[,1] < nr & index[,2] < nc,]
+        dists = apply(index, 1, function(i) conn[[3]][i[1], i[2]]     )
+        if (all(is.na(dists))) next
+        c(i, j) := index[which.min(dists),]
+        connA = conn
+        for (step in 1:1000) {
+            print(step)
+            txy = sample(c(0, 1), 1)
+            if (txy == 0) i = i + sample(c(-1, 1), 1)
+               else j = j + sample(c(-1, 1), 1)
+            
+            if (i > nr | j > nc | is.na(zscore[i,j])) break  
+            conn = findM(i, j, conn, zscore, TRUE)
+            
+            conn[[4]][i,j] = conn[[4]][i,j]+1
+        }    
+         print(step)
+    }
+    rout = rin
+    for (i in 1:4) rout[[i]][] = conn[[i]]    
+    browser()
+}
+conn1x = targetRW(conn1, zscore, 10)
+
+par(mfrow = c(2, 2), mar = rep(0, 4))
+plot_map_standrd(zscore1, zcols, zlimits, FALSE)
+add_raster_legend2(zcols, zlimits, dat = zscore,
+                   plot_loc = c(0.5, 0.97, 0.93, 0.95),
+                   add = TRUE, transpose = F, extend_max = T, extend_min = T, srt = 0)  
+plot_map_standrd(zscore2, zcols, zlimits, FALSE)
+plot_map_standrd(conn1[[3]], conn_cols, conn_limits, FALSE)
+points(start[1], start[2], pch = 4, cex = 3, lwd = 4) 
+add_raster_legend2(conn_cols, conn_limits, dat = conn0[[3]],
+                   plot_loc = c(0.5, 0.97, 0.93, 0.95),
+                   add = TRUE, transpose = F, extend_max = T, srt = 0)    
+plot_map_standrd(conn2[[3]], conn_cols, conn_limits, FALSE) 
+points(start[1], start[2], pch = 4, cex = 3, lwd = 4)  
+
+
+#for (chain in 1:1000) {
+#    print(chain)
+#    i = i0; j = j0
+#    for (step in 1:50000) {
+#        txy = sample(c(0, 1), 1)
+#        if (txy == 0) i = i + sample(c(-1, 1), 1)
+#        else j = j + sample(c(-1, 1), 1)
+#        
+#        if (is.na(zscore[i,j])) break
+#        conn[[4]][i,j] = conn[[4]][i,j]+1
+#    }
+#}
