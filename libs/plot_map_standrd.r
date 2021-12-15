95ebe801a51b46e731f7d479544b85f6fc2f5483
Douglas Kelley <douglas.i.kelley@gmail.com>
2021-12-15 09:16:38 +0000

    adpating for plotting from eother notebooks or hoem dir

--- libs/plot_map_standrd.r
+++ libs/plot_map_standrd.r
@@ -13,11 +13,15 @@ plot_map_standrd <- function(r, cols, limits, readyCut = TRUE, add_legend = FALS
     
     plot_map(cols)
     
-    mask = openResampleMask('data/seamask.nc')
+    seamaskFile = 'data/seamask.nc'; icemaskFile = 'data/icemask.nc'
+    if (!file.exists(seamaskFile)) {
+        seamaskFile = paste0('../', seamaskFile); icemaskFile = paste0('../', icemaskFile)
+    }
+    mask = openResampleMask(seamaskFile)
     contour(mask, add = TRUE, drawlabels = FALSE, lwd = 1)
 
     #plot_map(cols = make.transparent(cols,0.67), add = TRUE)
 
-    mask = openResampleMask('data/icemask.nc')
+    mask = openResampleMask(icemaskFile)
     plot_raster_from_raster(mask, col = c('transparent', '#CCCCCC'), limits = c(0.5), coast.lwd = NULL, add = TRUE, quick = TRUE, add_legend = FALSE)
 }
